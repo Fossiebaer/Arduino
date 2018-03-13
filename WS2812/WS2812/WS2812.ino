869e52b37dc6eec4b27f314eace83af6365efccd
Oliver Lenert <oliver.lenert@gmail.com>
2018-03-13 14:36:15 +0100

    Added some comments

--- WS2812/WS2812/WS2812.ino
+++ WS2812/WS2812/WS2812.ino
@@ -8,7 +8,7 @@ Author:	olli
 #include <NeoPixelAnimator.h>
 #include <dummy.h>
 #include <EspHomeBase.h>
-// the setup function runs once when you press reset or power the board
+
 
 EspHomeBase *_server;
 char tempBuf[TEMP_SIZE];
@@ -24,6 +24,7 @@ bool test = false;
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(numPixel, pinPixel);
 NeoPixelAnimator animations(numPixel, NEO_CENTISECONDS);
 
+// Stores the colorvalues and curve type for animations
 struct MyAnimationState {
 	RgbColor StartingColor;
 	RgbColor EndingColor;
@@ -32,6 +33,7 @@ struct MyAnimationState {
 
 MyAnimationState animationState[numPixel];
 
+// Updates the leds on every animation step
 void AnimUpdate(const AnimationParam &param) {
 	float progress = animationState[param.index].Easeing(param.progress);
 
@@ -42,6 +44,7 @@ void AnimUpdate(const AnimationParam &param) {
 	strip.SetPixelColor(param.index, updatedColor);
 }
 
+// Delivers the website for controlling the leds
 void handleGet(AsyncWebServerRequest  *req) {
 	readFile("/ws2812.html", tempBuf, TEMP_SIZE);
 	_server->replace(pageBuf, tempBuf);
@@ -50,13 +53,16 @@ void handleGet(AsyncWebServerRequest  *req) {
 	req->send(resp);
 }
 
+// Handles the POST request from the control website
 void handlePost(AsyncWebServerRequest *req) {
+	// if not yet done, start the led strip
 	if (!test) {
 		test = true;
 		strip.Begin();
 	}
+	// get first color value from request
 	const char *color = req->arg("color").c_str();
-	//strcpy(tempBuf, color + 1);
+	// convert string to RGB values
 	memcpy(tempBuf, color + 1, 2);
 	tempBuf[2] = '\0';
 	uint8_t r = strtol(tempBuf, NULL, 16);
@@ -65,10 +71,10 @@ void handlePost(AsyncWebServerRequest *req) {
 	uint8_t g = strtol(tempBuf, NULL, 16);
 	strncpy(tempBuf, color + 5, 3);
 	uint8_t b = strtol(tempBuf, NULL, 16);
-
 	RgbColor c(r, g, b);
+	// get second color from request
 	const char *color2 = req->arg("color2").c_str();
-	//strcpy(tempBuf, color + 1);
+	// convert string to RGB values
 	memcpy(tempBuf, color2 + 1, 2);
 	tempBuf[2] = '\0';
 	uint8_t r2 = strtol(tempBuf, NULL, 16);
@@ -79,15 +85,15 @@ void handlePost(AsyncWebServerRequest *req) {
 	uint8_t b2 = strtol(tempBuf, NULL, 16);
 	RgbColor c2(r2, g2, b2);
 
+	// Update all LEDs with new colors and start the animation
 	for (int i = 0; i < numPixel; i++) {
 		animationState[i].StartingColor = c;
 		animationState[i].EndingColor = c2;
 		animationState[i].Easeing = NeoEase::CubicInOut;
-		//delay(1);
 		animations.StartAnimation(i, 250, AnimUpdate);
 	}
-		
 	strip.Show();
+	// return control website to browser
 	readFile("/ws2812.html", tempBuf, TEMP_SIZE);
 	_server->replace(pageBuf, tempBuf);
 	AsyncWebServerResponse *resp = req->beginResponse(200, "text/html", pageBuf);
@@ -95,6 +101,7 @@ void handlePost(AsyncWebServerRequest *req) {
 	
 }
 
+// Replaces the placeholders in the website with the current color values
 int colorReplacer(char *buf, const char *needle) {
 	if (strcmp(needle, "%color%") == 0) {
 		sprintf(buf, "#%02x%02x%02x", animationState[0].StartingColor.R, animationState[0].StartingColor.G, animationState[0].StartingColor.B);
@@ -108,28 +115,36 @@ int colorReplacer(char *buf, const char *needle) {
 	}
 }
 
+// the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(115200);
 	Serial.println("");
 	
+	// get instance of EspHomeBase
 	_server = EspHomeBase::getInstance();
+	// wait till EspHomeBase is ready and set up
 	while (!EspHomeBase::ready) {
 		sleep(5);
 	}
+	// set connection mode of EspHomeBase. This is for testing only. Normally one would use MODE_MQTT or MODE_HTTP
 	_server->changeMode(MODE_AP_HTTP);
+
+	// register the control website with the webserver
 	_server->registerHttpCallback("/ws2812", HTTP_GET, handleGet);
 	_server->registerHttpCallback("/ws2812", HTTP_POST, handlePost);
+	// register the color replacer with the webserver
 	_server->registerReplacerCallback("%color%", colorReplacer);
 	_server->registerReplacerCallback("%color2%", colorReplacer);
-	//strip.Begin();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+	// if animation is started, continue with next step
 	if (animations.IsAnimating()) {
 		animations.UpdateAnimations();
 		strip.Show();
 	}
+	// if animation has ended, swap color values and restart animation
 	else if(test){
 		for (int i = 0; i < numPixel; i++) {
 			RgbColor temp = animationState[i].StartingColor;
