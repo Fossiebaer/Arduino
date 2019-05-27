6869a887c1b1eceee911848bf01c015686dafec3
Oliver Lenert <oliver.lenert@gmail.com>
2019-05-27 07:21:53 +0200

    New example HTTPSensor

---
+++ examples/HTTPSensor/HTTPSensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HTTPSensor.ino
+ Created:	27.05.2019 07:19:41
+ Author:	olli
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
